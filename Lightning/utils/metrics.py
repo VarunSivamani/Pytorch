e9bf89286d98413c99d3d10fb720c623063b0ca8
VarunSivamani <varunsivamani@gmail.com>
2023-08-05 20:20:53 +0530

    metrics.py file

---
+++ Lightning/utils/metrics.py
@@ -0,0 +1,31 @@
+import torch
+from torchmetrics import Metric
+from pytorch_lightning.callbacks import Callback
+
+
+class MyPrintingCallback(Callback):
+    def __init__(self):
+        super().__init__()
+
+    def on_train_start(self, trainer, pl_module):
+        print("Starting to train!")
+
+    def on_train_end(self, trainer, pl_module):
+        print("Training is done.")
+
+
+class MyAccuracy(Metric):
+    def __init__(self):
+        super().__init__()
+
+        self.add_state("total", default=torch.tensor(0), dist_reduce_fx="sum")
+        self.add_state("correct", default=torch.tensor(0), dist_reduce_fx="sum")
+
+    def update(self, preds, target):
+        preds = torch.argmax(preds, dim=1)
+        assert preds.shape == target.shape
+        self.correct += torch.sum(preds == target)
+        self.total += target.numel()
+
+    def compute(self):
+        return 100 * self.correct.float() / self.total.float()
