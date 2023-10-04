732e3ade267192cac4db7dc1e48632a39734bd57
VarunSivamani <varunsivamani@gmail.com>
2023-10-04 12:41:07 +0530

    added utils file

---
+++ CLIP/utils.py
@@ -0,0 +1,89 @@
+import torch
+from transformers import DistilBertModel, DistilBertConfig, DistilBertTokenizer
+from tqdm.autonotebook import tqdm
+from config import CFG
+from dataset import *
+from model import CLIPModel
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import torch.nn.functional as F
+
+def get_lr(optimizer):
+    for param_group in optimizer.param_groups:
+        return param_group["lr"]
+
+def make_train_valid_dfs():
+    dataframe = pd.read_csv(f"{CFG.captions_path}/captions.csv")
+    max_id = dataframe["id"].max() + 1 if not CFG.debug else 100
+    image_ids = np.arange(0, max_id)
+    np.random.seed(42)
+    valid_ids = np.random.choice(
+        image_ids, size=int(0.2 * len(image_ids)), replace=False
+    )
+    train_ids = [id_ for id_ in image_ids if id_ not in valid_ids]
+    train_dataframe = dataframe[dataframe["id"].isin(train_ids)].reset_index(drop=True)
+    valid_dataframe = dataframe[dataframe["id"].isin(valid_ids)].reset_index(drop=True)
+    return train_dataframe, valid_dataframe
+
+
+def build_loaders(dataframe, tokenizer, mode):
+    transforms = get_transforms(mode=mode)
+    dataset = CLIPDataset(
+        dataframe["image"].values,
+        dataframe["caption"].values,
+        tokenizer=tokenizer,
+        transforms=transforms,
+    )
+    dataloader = torch.utils.data.DataLoader(
+        dataset,
+        batch_size=CFG.batch_size,
+        num_workers=CFG.num_workers,
+        shuffle=True if mode == "train" else False,
+    )
+    return dataloader
+
+def get_image_embeddings(valid_df, model_path):
+    tokenizer = DistilBertTokenizer.from_pretrained(CFG.text_tokenizer)
+    valid_loader = build_loaders(valid_df, tokenizer, mode="valid")
+    
+    model = CLIPModel().to(CFG.device)
+    model.load_state_dict(torch.load(model_path, map_location=CFG.device))
+    model.eval()
+    
+    valid_image_embeddings = []
+    with torch.no_grad():
+        for batch in tqdm(valid_loader):
+            image_features = model.image_encoder(batch["image"].to(CFG.device))
+            image_embeddings = model.image_projection(image_features)
+            valid_image_embeddings.append(image_embeddings)
+    return model, torch.cat(valid_image_embeddings)
+
+def find_matches(model, image_embeddings, query, image_filenames, n=9):
+    tokenizer = DistilBertTokenizer.from_pretrained(CFG.text_tokenizer)
+    encoded_query = tokenizer([query])
+    batch = {
+        key: torch.tensor(values).to(CFG.device)
+        for key, values in encoded_query.items()
+    }
+    with torch.no_grad():
+        text_features = model.text_encoder(
+            input_ids=batch["input_ids"], attention_mask=batch["attention_mask"]
+        )
+        text_embeddings = model.text_projection(text_features)
+
+    image_embeddings_n = F.normalize(image_embeddings, p=2, dim=-1)
+    text_embeddings_n = F.normalize(text_embeddings, p=2, dim=-1)
+    dot_similarity = text_embeddings_n @ image_embeddings_n.T
+
+    values, indices = torch.topk(dot_similarity.squeeze(0), n * 5)
+    matches = [image_filenames[idx] for idx in indices[::5]]
+
+    _, axes = plt.subplots(3, 3, figsize=(10, 10))
+    for match, ax in zip(matches, axes.flatten()):
+        image = cv2.imread(f"{CFG.image_path}/{match}")
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        ax.imshow(image)
+        ax.axis("off")
+
+    plt.show()
